617cf6310b74c2cf5b287301f49a98b6f3c0b85d
Daniel Walkden-Brown <danielwalkdenbrown@gmail.com>
2019-07-04 12:02:58 +1000

    mobile version, handurger, favicon.

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-const getDimensionsForScale = (scale = 1) => {
-    return `64 128 8.574 96 8.574 32 64 0 119.426 32 119.426 96`;
-}
+// const getDimensionsForScale = (scale = 1) => {
+//     return `64 128 8.574 96 8.574 32 64 0 119.426 32 119.426 96`;
+// }
 
-document.addEventListener("DOMContentLoaded", function (event) {
-    anime({
-        targets: ['.shape polygon', 'feTurbulence', 'feDisplacementMap'],
-        points: getDimensionsForScale(2),
-        baseFrequency: 0,
-        scale: 1,
-        loop: true,
-        direction: 'alternate',
-        easing: 'easeInOutExpo'
-    });
-});
+// document.addEventListener("DOMContentLoaded", function (event) {
+//     anime({
+//         targets: ['.shape polygon', 'feTurbulence', 'feDisplacementMap'],
+//         points: getDimensionsForScale(2),
+//         baseFrequency: 0,
+//         scale: 1,
+//         loop: true,
+//         direction: 'alternate',
+//         easing: 'easeInOutExpo'
+//     });
+// });
+
+function onMobileMenuIconClick(mobileMenuIconElement) {
+    console.log('hello');
+    mobileMenuIconElement.classList.toggle('change');
+    const mobileMenu = document.getElementById('mobile-menu');
+    mobileMenu.classList.toggle('show')
+}
